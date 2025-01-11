1437327b25e749eedcd7a4f1a930eeeb35621f1b
rauan <ommirkulr@gmail.com>
2025-01-12 01:59:23 +0500

    minor changes

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -16,7 +16,10 @@ func ConfigLoad() Config {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
-	Cfg.ConnectionString = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	Cfg.ConnectionString = fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
+		host, port, user, password, dbname,
+	)
 	fmt.Println(Cfg.ConnectionString)
 	return Cfg
 }
